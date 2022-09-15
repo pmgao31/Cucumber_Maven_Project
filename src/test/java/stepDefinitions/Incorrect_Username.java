6c124b5e8023bc7a2f5925a7dd814e0a021eba85
pmgao31 <prasanth31mathi@gmail.com>
2022-09-15 22:53:12 +0530

    Deleting the report

--- src/test/java/stepDefinitions/Incorrect_Username.java
+++ src/test/java/stepDefinitions/Incorrect_Username.java
@@ -3,7 +3,6 @@ package stepDefinitions;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
